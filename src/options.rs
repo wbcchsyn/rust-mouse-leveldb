9bcfda70b1fec4ac51f7c4b46c1e6a13b0756dc0
Yoshida Shin <wbcchsyn@gmail.com>
2021-02-03 17:47:51 +0900

    Create method 'options::Options::as_ptr()'

--- src/options.rs
+++ src/options.rs
@@ -80,4 +80,9 @@ impl Options {
             Self(NonNull::new_unchecked(ptr))
         }
     }
+
+    /// Provides a raw pointer to wrapped address.
+    pub fn as_ptr(&self) -> *const leveldb_options_t {
+        self.0.as_ptr()
+    }
 }
